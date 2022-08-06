96179eef540ea327611877c71c5560006bcb00e9
unubtu kilke <kilkenny116@gmail.com>
2022-08-06 21:17:18 +0300

    gitignore

--- src/app/Helpers/ErrorHelper.php
+++ src/app/Helpers/ErrorHelper.php
@@ -6,7 +6,7 @@ class ErrorHelper
 {
   public static function writeToFileAndDie($text)
   {
-    file_put_contents('ExceptionLog', $text, FILE_APPEND);
+    file_put_contents('ExceptionLog.log', $text, FILE_APPEND);
     echo $text;
     die();
   }
