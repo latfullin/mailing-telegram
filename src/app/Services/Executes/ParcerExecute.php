50bdc75f7d9296fdc8632fac6c13521e58b6f5ef
unubtu kilke <kilkenny116@gmail.com>
2022-08-10 20:39:37 +0300

    parcer

--- src/app/Services/Executes/ParcerExecute.php
+++ src/app/Services/Executes/ParcerExecute.php
@@ -2,14 +2,24 @@
 
 namespace App\Services\Executes;
 
+use App\Helpers\ErrorHelper;
 use App\Helpers\WorkingFileHelper;
 use App\Services\Authorization\Telegram;
+use Exception;
 
 class ParcerExecute extends Execute
 {
   private static ?ParcerExecute $instance = null;
+  const MAX_USER = 10000;
+
   const CONSONANTS_LETTERS = ['Б', 'В', 'Г', 'Д', 'Ж', 'З', 'Й', 'К', 'Л', 'М', 'Н', 'П', 'Р', 'С', 'Т', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ'];
+
   const MALE_END_LETTERS = ['й', 'ь', 'а', 'я'];
+
+  const ALPHABETS = [
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'э', 'ю', 'я']
+  ];
 
   /**
    * limit count, on the get users in request
@@ -64,37 +74,50 @@ class ParcerExecute extends Execute
 
   /**
    * divite participant for gender
+   * not implemented
    */
   protected bool $divideGender = false;
 
   /**
    * need for user id, if not found username
    */
-  protected bool $needUsersId = true;
+  protected bool $needUsersId;
+  protected bool $needBreakTime;
 
-  protected array $data = [
-    'oneDay' => [],
-    'twoDay' => [],
-    'threeDay' => [],
-    'oneWeek' => [],
-    'oneMonth' => [],
-    'moreOneMonth' => [],
-    'notTime' => [],
-  ];
+  protected array $data = [];
 
-  protected int $countUsers = 0;
+  /**
+   * @param now time unix format
+   */
+  protected int $now;
+  protected int $lengthArrayParticipants = 0;
+  protected int $countParticipants;
 
-  private function __construct(bool $needUsersId)
+  private function __construct(bool $needUsersId, bool $needBreakTime)
   {
     parent::__construct();
     $this->needUsersId = $needUsersId;
+    $this->needBreakTime = $needBreakTime;
+
+    if ($this->needBreakTime) {
+      $this->data = [
+        'oneDay' => [],
+        'twoDay' => [],
+        'threeDay' => [],
+        'oneWeek' => [],
+        'oneMonth' => [],
+        'moreOneMonth' => [],
+        'notTime' => [],
+      ];
+    }
   }
 
-  public static function instance(bool $needUsersId = true): ParcerExecute
+  public static function instance(bool $needUsersId = true,  bool $needBreakTime = true): ParcerExecute
   {
     if (self::$instance === null) {
-      self::$instance = new self($needUsersId);
+      self::$instance = new self($needUsersId, $needBreakTime);
     }
+
     return self::$instance;
   }
 
@@ -106,36 +129,83 @@ class ParcerExecute extends Execute
 
     $this->channel = $channel;
     $this->channelInformation = $this->verifyChannel($channel);
-    $this->countAmountInteration($this->channelInformation['full_chat']['participants_count']);
+    $this->countParticipants = $this->channelInformation['full_chat']['participants_count'];
 
     return $this;
   }
 
-  public function collectParticipants(): ParcerExecute
+  public function executes(): bool
   {
-    $offset = 0;
-    // $result = [];
-    for ($i = 0; $i < $this->countCycles; $i++) {
-      echo $i;
-      $result = Telegram::instance('79776782207')->getParticipants($this->channel, $offset)['users'];
-      for ($r = 0; $r < count($result); $r++) {
-        $this->participants[$result[$r]['id']] = $result[$r];
+    $this->checkChannelInformation();
+    if ($this->countParticipants > self::MAX_USER) {
+      $this->collectParticipants($this->countParticipants);
+    } else {
+      $this->bigChannel();
+    }
+    $this->usersProcessing();
+
+    return true;
+  }
+
+  public function bigChannel()
+  {
+    foreach (self::ALPHABETS as $key => $alphabets) {
+      foreach ($alphabets as $alphabet) {
+        echo $alphabet;
+
+        $countsParticipants = Telegram::instance('79874018497')->getParticipants($this->channel, 0, 1, q: $alphabet)['count'];
+        $countsParticipants = $countsParticipants > 10000 ? 10000 : $countsParticipants;
+        $this->collectParticipants($countsParticipants, $alphabet);
       }
-      $offset += SELF::OFFSET_LIMIT;
     }
 
-    $this->countUsers = count($this->participants);
+    $handle = fopen('test.txt', 'a');
+    foreach ($this->participants as $participant) {
+      $id = '';
+      if ($participant['username'] ?? false) {
+        $id = $participant['username'];
+      } else {
+        $id = $participant['id'];
+      }
+      $time = $participant['status']['was_online'] ?? false;
+      fwrite($handle, "id:{$id};time:{$time}\n");
+    }
+    $this->resetData();
+    $this->lengthArrayParticipants = 0;
 
-    return $this;
+    fclose($handle);
   }
 
-  public function breakInfoTime(): void
+  public function collectParticipants(int $countUsers, $q = ''): bool
   {
-    $now = time();
+    $result = [];
+    $this->countAmountInteration($countUsers);
+
+    for ($i = 0; $i < $this->countCycles; $i++) {
+      $result[] = Telegram::instance('79874018497')->getParticipants($this->channel, $i * self::OFFSET_LIMIT, q: $q)['users'];
+    }
+
+    echo $i * self::OFFSET_LIMIT;
+    for ($r = 0; $r < count($result); $r++) {
+      for ($s = 0; $s < count($result[$r]); $s++) {
+        if ($result[$r][$s]['username'] ?? false || $this->needUsersId) {
+          $this->participants[$result[$r][$s]['id']] = $result[$r][$s];
+        }
+      }
+    }
+
+    $this->lengthArrayParticipants += count($this->participants);
+
+    return true;
+  }
+
+  public function usersProcessing(): void
+  {
+    $this->now = time();
 
     foreach ($this->participants as $participant) {
       $time = $participant['status']['was_online'] ?? false;
-      $userNameOrId = 'empty';
+      $userNameOrId = '';
       if ($participant['username'] ?? false) {
         $userNameOrId = '@' . $participant['username'];
       } else {
@@ -145,32 +215,42 @@ class ParcerExecute extends Execute
         $userNameOrId = $participant['id'];
       }
 
-      if ($time === false) {
-        $this->data['notTime'][] = $userNameOrId;
+      if ($this->needBreakTime) {
+        $this->breakTime($userNameOrId, $time);
         continue;
       }
 
-      $validTime = $now - $time;
+      $this->data[] = $userNameOrId;
+    }
+  }
 
-      switch ($validTime) {
-        case self::ONE_DAY > $validTime:
-          $this->data['oneDay'][] = $userNameOrId;
-          break;
-        case self::ONE_DAY < $validTime &&  self::TWO_DAY > $validTime:
-          $this->data['twoDay'][] = $userNameOrId;
-          break;
-        case self::TWO_DAY < $validTime && self::THREE_DAY > $validTime:
-          $this->data['threeDay'][] = $userNameOrId;
-          break;
-        case self::THREE_DAY < $validTime && self::ONE_WEEK > $validTime:
-          $this->data['oneWeek'][] = $userNameOrId;
-          break;
-        case self::ONE_WEEK < $validTime && self::ONE_MONTH > $validTime:
-          $this->data['oneMonth'][] = $userNameOrId;
-          break;
-        default:
-          $this->data['moreOneMonth'][] = $userNameOrId;
-      }
+  private function breakTime($userNameOrId, $time): void
+  {
+    if ($time === false) {
+      $this->data['notTime'][] = $userNameOrId;
+      return;
+    }
+
+    $validTime = $this->now - $time;
+
+    switch ($validTime) {
+      case self::ONE_DAY > $validTime:
+        $this->data['oneDay'][] = $userNameOrId;
+        break;
+      case self::ONE_DAY < $validTime &&  self::TWO_DAY > $validTime:
+        $this->data['twoDay'][] = $userNameOrId;
+        break;
+      case self::TWO_DAY < $validTime && self::THREE_DAY > $validTime:
+        $this->data['threeDay'][] = $userNameOrId;
+        break;
+      case self::THREE_DAY < $validTime && self::ONE_WEEK > $validTime:
+        $this->data['oneWeek'][] = $userNameOrId;
+        break;
+      case self::ONE_WEEK < $validTime && self::ONE_MONTH > $validTime:
+        $this->data['oneMonth'][] = $userNameOrId;
+        break;
+      default:
+        $this->data['moreOneMonth'][] = $userNameOrId;
     }
   }
 
@@ -179,26 +259,57 @@ class ParcerExecute extends Execute
     $this->countCycles = ceil($informationChannel / self::OFFSET_LIMIT);
   }
 
+  private function saveToFile(): void
+  {
+    WorkingFileHelper::saveForFileTask($this->task, "Группа {$this->channel}\n\n");
+    foreach ($this->data as $key => $items) {
+      if (is_array($items)) {
+        $lang = timeLang($key);
+        WorkingFileHelper::saveForFileTask($this->task, "\n{$lang}\n\n");
+        foreach ($items as $item) {
+          WorkingFileHelper::saveForFileTask($this->task, "$item\n");
+        }
+        continue;
+      }
+
+      WorkingFileHelper::saveForFileTask($this->task, "$items\n");
+    }
+  }
+
   private function resetData(): void
   {
     $this->participants = [];
   }
 
-  private function saveToFile()
+  public function save(): bool
   {
-    WorkingFileHelper::saveForFileTask($this->task, "Группа {$this->channel}\n\n");
-    WorkingFileHelper::saveForFileTask($this->task, "Количество пользователей в группе: {$this->countUsers}\n\n");
-    foreach ($this->data as $key => $days) {
-      $lang = timeLang($key);
-      WorkingFileHelper::saveForFileTask($this->task, "\n{$lang}\n\n");
-      foreach ($days as $item) {
-        WorkingFileHelper::saveForFileTask($this->task, "$item\n");
-      }
+    if ($this->data) {
+      $this->saveToFile();
     }
+
+    return true;
   }
 
   public function __destruct()
   {
     $this->saveToFile();
+  }
+
+  public function setNeedUserId(bool $bool): ParcerExecute
+  {
+    $this->needUsersId = $bool;
+
+    return $this;
+  }
+
+  private function checkChannelInformation()
+  {
+    try {
+      if (!$this->channelInformation) {
+        throw new Exception('Not information for channel');
+      }
+    } catch (Exception $e) {
+      ErrorHelper::writeToFileAndDie($e);
+    }
   }
 }
