9281eea48b29c97a2172e69c821a3a7db8a84da2
unubtu kilke <kilkenny116@gmail.com>
2022-08-19 00:03:07 +0300

    new phone

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'src/kernel.php';
 
+use App\Controllers\ParserController;
+use App\Controllers\WakeUpAccountsController;
 use App\Services\Authorization\Telegram;
 use App\Services\Executes\EditProfileExecute;
 use App\Services\Executes\ParserTelephoneExecute;
@@ -11,5 +13,17 @@ use App\Services\WarmingUp\AccountWarmingUp;
 
 // $a = new ParserTelephoneExecute(false, false);
 
-$parser = new ParserTelephoneExecute(true, true);
-$parser->checkPhones(['79874018497', '+79270370406'])->usersProcessing()->saveToFile();
+
+// $parser = new ParserTelephoneExecute(true, true);
+// $parser->checkPhones(['79874018497', '+79270370406'])->usersProcessing()->saveToFile();
+
+
+// $a = new ParserController();
+// $a->parseGroup('https://t.me/laravel_web');
+
+
+$a = new WakeUpAccountsController();
+$a->joinChannel('https://t.me/+vZ57IK_pcghiOWZi');
+// $a->warmingUpAccount();
+// $a->wakeUpAccounts();
+// Telegram::instance('79299204367')->autorizationSession();
