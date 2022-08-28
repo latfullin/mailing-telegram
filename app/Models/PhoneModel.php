7d1ed5e05520bb2c32db9d61942760aff2041006
unubtu kilke <kilkenny116@gmail.com>
2022-08-29 01:23:14 +0300

    add methods for model phone

--- app/Models/PhoneModel.php
+++ app/Models/PhoneModel.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
-class PhoneModel extends Model {
+class PhoneModel extends Model
+{
 
   protected $table = 'sessions';
 
   public function __construct()
   {
     return parent::__construct($this->table);
+  }
+
+  public function increment(string $phone, string $column)
+  {
+    return $this->connect->query("UPDATE {$this->table} SET {$column} = {$column} + 1 WHERE phone = {$phone}");
+  }
+
+  public function sessionList($column = 'count_action', int $limit = 10)
+  {
+    return $this->connect->query("SELECT * FROM {$this->table}  WHERE ban = '0' AND {$column} < {$limit}  ORDER BY {$column}")->fetchAll();
   }
 }
