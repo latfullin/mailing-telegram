18d1d1b6e4dd5a896c3d7a9d8c847f87e34e0ca1
unubtu kilke <kilkenny116@gmail.com>
2022-09-07 22:15:41 +0300

    add contriller autorization

--- app/Controllers/AutorizationController.php
+++ app/Controllers/AutorizationController.php
@@ -4,17 +4,31 @@ namespace App\Controllers;
 
 use App\Helpers\ArgumentsHelpers;
 use App\Models\PhoneModel;
-use App\Services\Authorization\Telegram;
+use App\Services\Authorization\TelegramAuthorization;
 
 class AutorizationController
 {
+  private $createSession = false;
   public function createSession(ArgumentsHelpers $argumets, PhoneModel $phone)
   {
-    $findSession = $phone->where(["phone" => $argumets->phone])->first();
-    if (empty($findSession)) {
-      $phone->insert(["phone" => $argumets->phone]);
-    }
+    try {
+      if (!$this->createSession) {
+        $findSession = $phone->where(["phone" => $argumets->phone])->first();
+        if (empty($findSession)) {
+          $phone->insert(["phone" => $argumets->phone]);
+        }
+        $this->createSession = true;
+        $telegram = new TelegramAuthorization(
+          $argumets->phone,
+          $argumets->proxy
+        );
+      }
 
-    Telegram::instance($argumets->phone)->autorizationSession();
+      $telegram->autorizationSession(
+        "Hello how dodasdas 412das da sd12312 asd asd yo do? "
+      );
+    } catch (\Exception $e) {
+      $telegram->autorizationSession("My fristasd sas");
+    }
   }
 }
