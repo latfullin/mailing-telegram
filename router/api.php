15b6a9aa110120abae1210562961e3b0c2d3572f
unubtu kilke <kilkenny116@gmail.com>
2022-09-23 23:30:51 +0300

    add routers

--- router/api.php
+++ router/api.php
@@ -1,3 +1,12 @@
-// new Providers(AutorizationController::class, "createSession", [
-//   "phone" => 6282187155900,
-// ]);
+<?php
+
+use App\Controllers\AutorizationController;
+use App\Controllers\SendController;
+use App\Routers\Router;
+
+Router::post("/api/created-session", [
+  AutorizationController::class,
+  "createSession",
+]);
+
+Router::post("/api/send-message", [SendController::class, "sendMessage"]);
---
+++ router/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Routers\Router;
+
+Router::get("/get", [Router::class]);
+Router::get("/class", [Router::class]);
+Router::get("/sfasfs", [Router::class]);
