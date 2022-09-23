1fc83ed8eca171d5524d59daa518ee639417f1f2
unubtu kilke <kilkenny116@gmail.com>
2022-09-24 01:40:29 +0300

    page send-message

---
+++ resources/components/pages/send-message.php
@@ -0,0 +1,24 @@
+<div class="px-2">
+  <h2 class="fs-2">Написать сообщение</h2>
+  <form class="col-3 py-1" action="/api/send-message" method="post">
+    <div class="mb-1">
+      <label class="form-label" for='phone'>
+        Номер телефона сессии
+      </label>
+      <input class="form-control" type="text" name="phone" id="phone">
+    </div>
+    <div class="mb-1">
+      <label class="form-label" for='phone'>
+        Кому:
+      </label>
+      <input class="form-control" type="text" name="how" id="">
+    </div>
+    <div class="mb-2">
+      <label class="form-label" for='phone'>
+        Сообщение:
+      </label>
+      <input class="form-control" type="text" name="msg" id="">
+    </div>
+    <button class="btn border btn-outline-secondary" type="submit">Отрпавить</button>
+  </form>
+</div>
