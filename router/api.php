acc02bab9ff12ee3876e9771fc809e53daacdc99
unubtu kilke <kilkenny116@gmail.com>
2022-09-25 23:11:21 +0300

    add api path

--- router/api.php
+++ router/api.php
@@ -1,13 +1,17 @@
 <?php
 
 use App\Controllers\AutorizationController;
+use App\Controllers\InvitationsController;
 use App\Controllers\MailingMessagesController;
+use App\Controllers\ParserController;
 use App\Controllers\ProxyController;
 use App\Controllers\SendController;
 use App\Routers\Router;
 
 Router::post('/api/created-session', [AutorizationController::class, 'createSession']);
 Router::post('/api/send-message', [SendController::class, 'sendMessage']);
+Router::post('/api/parser-channel', [ParserController::class, 'parseGroup']);
 Router::post('/api/buy-proxy', [ProxyController::class, 'buyProxy']);
 Router::post('/api/proxy-check-active', [ProxyController::class, 'checkActiveProxy']);
 Router::post('/api/created-task-mailing', [MailingMessagesController::class, 'createTaskMailingMessages']);
+Router::post('/api/created-task-invitations', [InvitationsController::class, 'createTaskInvitations']);
