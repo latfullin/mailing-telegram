7f938209b5e13d4acd1557b0cdf9bf5e96560175
unubtu kilke <kilkenny116@gmail.com>
2022-09-25 23:13:20 +0300

    form bye proxy

--- resources/components/pages/proxy.php
+++ resources/components/pages/proxy.php
@@ -2,7 +2,7 @@
   <h1 class="fs-2">Прокси</h1>
   <div class="container" >
     <div class="row ul">
-      <? include_once "{$_SERVER['DOCUMENT_ROOT']}/resources/components/form/buy-proxy.php"?>
+      <? include_once "{$_SERVER['DOCUMENT_ROOT']}/resources/components/form/form-buy-proxy.php"?>
       <? include_once "{$_SERVER['DOCUMENT_ROOT']}/resources/components/proxy/count-active-proxy.php"?>
     </div>
   </div>
