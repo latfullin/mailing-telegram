3423cb68f004f01e8216d257fe8fec8a4905da52
unubtu kilke <kilkenny116@gmail.com>
2022-09-25 23:11:48 +0300

    created task

---
+++ resources/components/form/form-created-task-mailing.php
@@ -0,0 +1,16 @@
+<form action="/api/created-task-mailing" method="POST">
+  <label for="msg">
+    Сообщение:
+  </label>
+  <input class="form-control" type="text" name="msg" id="msg">
+  <label for="period">
+    Список пользователей:
+  </label>
+  <div class="input-group mb-1">
+    <div class="form-floating">
+      <textarea class="form-control" placeholder="Leave a comment here" name="users" id="users" style="height: 100px"></textarea>
+      <label for="floatingTextarea2">Пользователи</label>
+    </div>
+  </div>
+  <button class="btn border btn-outline-secondary" type="submit">Создать</button>
+</form>
--- resources/components/pages/created-task.php
+++ resources/components/pages/created-task.php
@@ -1,25 +1,16 @@
-<form action="/api/created-task-mailing" method="POST">
-  <label for="country">
-    Тип задания:
-  </label>
-  <div class="input-group mb-1">
-    <select class="form-select" name="country" id="country">
-      <option value="send_message">Рассылка сообщений</option>
-      <option value="invitations_channel">Инвайтинг</option>
-    </select>
-  </div>
-  <label for="msg">
-    Сообщение:
-  </label>
-  <input class="form-control" type="text" name="msg" id="msg">
-  <label for="period">
-    Список пользователей:
-  </label>
-  <div class="input-group mb-1">
-    <div class="form-floating">
-      <textarea class="form-control" placeholder="Leave a comment here" name="users" id="users" style="height: 100px"></textarea>
-      <label for="floatingTextarea2">Пользователи</label>
-    </div>
-  </div>
-  <button class="btn border btn-outline-secondary" type="submit">Создать</button>
-</form>
+<label for="type">
+  Тип задания:
+</label>
+<div class="input-group mb-1">
+  <select class="form-select" id="type">
+    <option value="send_message">Рассылка сообщений</option>
+    <option value="invitations_channel">Инвайтинг</option>
+  </select>
+</div>
+
+<div class="">
+  <? include_once "{$_SERVER['DOCUMENT_ROOT']}/resources/components/form/form-created-task-mailing.php" ?>
+</div>
+<div class="">
+  <? include_once "{$_SERVER['DOCUMENT_ROOT']}/resources/components/form/form-created-task-invitations.php" ?>
+</div>
