6dd9db1f872f189eb73e46fb7937e484720e4fbb
unubtu kilke <kilkenny116@gmail.com>
2022-09-25 23:15:51 +0300

    validated data

--- app/Controllers/ParserController.php
+++ app/Controllers/ParserController.php
@@ -9,23 +9,21 @@ use App\Services\Executes\ParserTelephoneExecute;
 
 class ParserController
 {
-  public function checkPhone(
-    ArgumentsHelpers $arg,
-    ParserTelephoneExecute $parser,
-    TelegramBot $telegram
-  ): void {
+  public function checkPhone(ArgumentsHelpers $arg, ParserTelephoneExecute $parser, TelegramBot $telegram): void
+  {
     $filePath = $parser
       ->checkPhones($arg->phones)
       ->usersProcessing()
       ->save();
+
     $telegram->setChatId($arg->userId)->sendFile($filePath);
   }
 
-  public function parseGroup(
-    ArgumentsHelpers $arg,
-    ParserExecute $parser,
-    TelegramBot $telegram
-  ): void {
+  public function parseGroup(ArgumentsHelpers $arg, ParserExecute $parser, TelegramBot $telegram): bool
+  {
+    if (!$arg->userId || !$arg->channel) {
+      return false;
+    }
     $filePath = $parser
       ->channel($arg->channel)
       ->executes()
