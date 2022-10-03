e421a305ff5ab5083cb75be0738cb93961164ba2
unubtu kilke <kilkenny116@gmail.com>
2022-10-03 23:51:34 +0300

    add services for start app

--- app/kernel.php
+++ app/kernel.php
@@ -7,5 +7,6 @@ class kernel
   protected array $services = [
     'sessions' => \App\Helpers\Sessions\Session::class,
     'cokkie' => \App\Helpers\Sessions\Cokkie::class,
+    'access' => \App\Helpers\Access::class,
   ];
 }
